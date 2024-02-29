408edc9a69d99923f1ff0ba3a5d750aba8e188cb
Kefsner <kesleyraimundo@gmail.com>
2024-02-29 17:56:03 -0300

    Abstract Index and Vertex Buffers

--- Universe/Source/Universe/Core/Application.h
+++ Universe/Source/Universe/Core/Application.h
@@ -9,6 +9,7 @@
 #include "Universe/ImGui/ImGuiLayer.h"
 
 #include "Universe/Renderer/Shader.h"
+#include "Universe/Renderer/Buffer.h"
 
 namespace Universe {
 
@@ -36,8 +37,10 @@ namespace Universe {
 		bool m_IsRunning = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	};
 
 	// By keeping this function outside of the application class,
---
+++ Universe/Source/Universe/Renderer/Renderer.cpp
@@ -0,0 +1,7 @@
+#include "UEpch.h"
+#include "Renderer.h"
+
+namespace Universe {
+
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
