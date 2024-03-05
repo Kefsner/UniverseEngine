c896cd423066e24281d59b7987740315ff4eef9c
Kefsner <kesleyraimundo@gmail.com>
2024-03-04 22:51:02 -0300

    Add Verrtex Array classes

--- Universe/Source/Universe/Core/Application.h
+++ Universe/Source/Universe/Core/Application.h
@@ -10,6 +10,7 @@
 
 #include "Universe/Renderer/Shader.h"
 #include "Universe/Renderer/Buffer.h"
+#include "Universe/Renderer/VertexArray.h"
 
 namespace Universe {
 
@@ -37,10 +38,11 @@ namespace Universe {
 		bool m_IsRunning = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 	};
 
 	// By keeping this function outside of the application class,
